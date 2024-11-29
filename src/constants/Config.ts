a2ba4d4ae8a275df29160c711385652de6f701c8
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-11-29 16:18:25 -0500

    feat: configuration Update

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,5 +1,14 @@
 export enum Config {
+    //tasa de interes
     INTEREST_RATE = "INTEREST_RATE",
+    //maximo credito
+    MAX_CREDIT_AMOUNT = "MAX_CREDIT_AMOUNT",
+    //minimo credito
+    MIN_CREDIT_AMOUNT = "MIN_CREDIT_AMOUNT",
+    //frecuencia de alertas
+    ALERT_FREQUENCY = "ALERT_FREQUENCY",
+    //firma
+    SIGNATURE = "SIGNATURE"
 }
 
 export enum ExpressServerConfig {
